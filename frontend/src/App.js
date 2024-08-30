1c9e260fbdbaf6e5c0fcf327e05d653da6ad8025
John Prathap Singh <220701112@rajalakshmi.edu.in>
2024-08-30 18:07:35 +0530

    Dashboard is completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import LoginForm from './components/Login_Page/Login';
 import SignupForm from './components/Signup_Page/Signup';
 import Dashboard from './components/Dashboard/Dashboard';
 // import { Forgot } from './components/Forgot_Password/Forgot';
 import WebSocketClient from './components/Websocket/websocket';
-import Settings from './components/Bar/Settings';
+import Settings from './components/Bar/Setting';
 import CollabWork from './components/Bar/CollabWork';
 import Upgrade from './components/Bar/Upgrade';
-import UserProfile from './components/Bar/Userprofile';
+import UserProfile from './components/Bar/UserProfile';
 import Sidebar from './components/Side_bar/Sidebar';
 //import './App.css';
 
 const App = () => {
   return (
     <Router>
+      <AppContent />
+    </Router>
+  );
+};
+
+const AppContent = () => {
+  const location = useLocation();
+
+  // Determine whether to show the sidebar
+  const showSidebar = location.pathname !== '/' && location.pathname !== '/login' && location.pathname !== '/signup';
+
+  return (
+    <div style={{ display: 'flex', height:'auto' }}>
+      {showSidebar && <Sidebar />}
       <WebSocketClient />
       <Routes>
         <Route path='/' element={<SignupForm />} /> {/*navigate to Signup page */}
@@ -22,8 +36,12 @@ const App = () => {
         <Route path='/signup' element={<SignupForm />} />
         <Route path='/dashboard' element={<Dashboard />} />
         {/* <Route path='/forgot' element={<Forgot />} /> navigate to Forgot Password page */}
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/collab-work" element={<CollabWork />} />
+        <Route path="/upgrade" element={<Upgrade />} />
+        <Route path="/profile" element={<UserProfile />} />
       </Routes>
-    </Router>
+    </div>
   );
 };
 export default App;

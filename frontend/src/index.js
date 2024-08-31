bbad69a3ec91cc794ddda50df39908c340415810
John Prathap Singh <220701112@rajalakshmi.edu.in>
2024-08-31 18:14:35 +0530

    Combined both admin_dashboard and server.py

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,7 @@ import UploadPage from './components/Upload/Upload';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UploadPage />
+    <App />
   </React.StrictMode>
 );
 
